a00ffc56f5937fd01f1cae8443189796636ce4d9
Anil Deshpande <anil.deshpande@shell.com>
2021-02-11 17:37:37 +0530

    non working  code

--- app/src/main/java/uithreadsdemo/youtube/com/uithreaddemo/MainActivity.java
+++ app/src/main/java/uithreadsdemo/youtube/com/uithreaddemo/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //workRequest = OneTimeWorkRequest.from(RandomNumberGeneratorWorker.class);
 
-        workRequest1 = 
+        
 
 
     }
